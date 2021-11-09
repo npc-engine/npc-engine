2a650720d0bdd4212df18f9e6edb2725d0070dcc
eublefar <evil.unicorn1@gmail.com>
2021-11-09 15:41:07 +0100

    Refactored tests into unit and integration tests

---
+++ tests/unit/test_text_gen.py
@@ -0,0 +1,48 @@
+"""
+.. currentmodule:: test_example
+.. moduleauthor:: evil.unicorn1 <evil.unicorn1@gmail.com>
+
+Speech synthesis test.
+"""
+import os
+import simpleaudio as sa
+from inference_engine.models import Model
+import time
+import logging
+
+
+def test_reply_default():
+    """Check if chatbot works"""
+    chatbot_model = Model.load(
+        os.path.join(
+            os.path.dirname(__file__),
+            "..\\..\\inference_engine\\resources\\models\\bart",
+        )
+    )
+    start = time.time()
+    answer = chatbot_model.generate_reply(
+        context=dict(
+            persona="""
+_setting_name Brimswood pub, Tavern
+_setting_desc The Brimswood pub is an old establishment. 
+It is sturdy, has a lot of life in its walls, but hasn't been updated in decades.
+The clientele are the same as they always are, and they don't see very many strangers. 
+The vibe is somber, and conversations are usually had in hushed tones.</s>
+<speaker_self>_self_name pet dog
+_self_persona I am mans best friend and I wouldn't have it any other way. I tend to my master and never leave his side. 
+I sleep at his feet and guard the room at night from things that go bump in the night.</s>
+<speaker_other>_partner_name the town baker's husband
+_other_persona I am the town baker's husband and I love eating pastries.  
+I tend to be in very good spirits and enjoy selling delicious baked goods that my wife has made.  
+My wife is great at baking but she is lousy at washing my clothes.  
+They keep shrinking!
+        """.strip(),
+            history=["<speaker_other>Hello friend!"],
+        ),
+        temperature=0.8,
+        topk=None,
+    )
+    end = time.time()
+    assert answer is not None
+    print("Answer: {}".format(answer))
+    print("done in {} seconds".format(end - start))
---
+++ tests/unit/test_zmq_server.py
@@ -0,0 +1,116 @@
+"""ZMQ server test."""
+import json
+from inference_engine.zmq_server import ZMQServer
+
+
+class StubSocket:
+    pass
+
+
+def test_zmq_json_rpc_server():
+    server = ZMQServer("5555")
+
+    print("Should reveive dummy")
+
+    def recv_string(self):
+        return json.dumps(
+            {"jsonrpc": "2.0", "method": "dummy", "id": 0, "params": ["foo", "bar"]}
+        )
+
+    def send_string(self, string: str):
+        message = json.loads(string)
+        assert message["id"] == 0
+        assert message["jsonrpc"] == "2.0"
+        assert "result" in message
+        raise KeyboardInterrupt()
+
+    StubSocket.send_string = send_string
+    StubSocket.recv_string = recv_string
+    stub = StubSocket()
+
+    server.socket = stub
+    api_dict = {
+        "dummy": lambda a, b: print(f" dummy API call with \na: {a}\nb: {b}"),
+        "dummy1": lambda a, b, c: print(
+            f" dummy1 API call with \na: {a}\nb: {b}\nc: {c}"
+        ),
+    }
+    try:
+        server.run(api_dict)
+    except KeyboardInterrupt:
+        pass
+
+
+def test_zmq_json_rpc_server1():
+    server = ZMQServer("5555")
+
+    print("Should reveive dummy1")
+
+    def recv_string(self):
+        return json.dumps(
+            {
+                "jsonrpc": "2.0",
+                "method": "dummy1",
+                "id": 0,
+                "params": ["foo", "bar", "baz"],
+            }
+        )
+
+    def send_string(self, string: str):
+        message = json.loads(string)
+        assert message["id"] == 0
+        assert message["jsonrpc"] == "2.0"
+        assert "result" in message
+        raise KeyboardInterrupt()
+
+    StubSocket.send_string = send_string
+    StubSocket.recv_string = recv_string
+    stub = StubSocket()
+
+    server.socket = stub
+    api_dict = {
+        "dummy": lambda a, b: print(f" dummy API call with \na: {a}\nb: {b}"),
+        "dummy1": lambda a, b, c: print(
+            f" dummy1 API call with \na: {a}\nb: {b}\nc: {c}"
+        ),
+    }
+    try:
+        server.run(api_dict)
+    except KeyboardInterrupt:
+        pass
+
+
+def test_zmq_json_rpc_server_incorrect_signature():
+    server = ZMQServer("5555")
+
+    def recv_string(self):
+        return json.dumps(
+            {
+                "jsonrpc": "2.0",
+                "method": "dummy",
+                "id": 0,
+                "params": ["foo", "bar", "baz"],
+            }
+        )
+
+    def send_string(self, string: str):
+        message = json.loads(string)
+        assert message["id"] == 0
+        assert message["jsonrpc"] == "2.0"
+        assert "error" in message
+        raise KeyboardInterrupt()
+
+    StubSocket.send_string = send_string
+    StubSocket.recv_string = recv_string
+    stub = StubSocket()
+    server.socket = stub
+
+    def incorrect_signature(foo):
+        print(f"foo {foo}")
+
+    api_dict = {"dummy": lambda *args, **kwargs: incorrect_signature(*args, **kwargs)}
+    try:
+        server.run(api_dict)
+    except KeyboardInterrupt:
+        pass
+
