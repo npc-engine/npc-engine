11d5dfdebbab941329e018a96927b669da7b7403
eublefar <evil.unicorn1@gmail.com>
2022-03-25 17:09:55 +0100

    Update requirements and version

--- npc_engine/version.py
+++ npc_engine/version.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-This module contains project version information.
+"""This module contains project version information."""
 
-.. currentmodule:: npc_engine.version
-.. moduleauthor:: evil.unicorn1 <evil.unicorn1@gmail.com>
-"""
-
-__version__ = "0.0.1"  #: the working version
-__release__ = "0.0.1"  #: the release version
+__version__ = "0.1.0"  #: the working version
+__release__ = "0.0.2"  #: the release version
