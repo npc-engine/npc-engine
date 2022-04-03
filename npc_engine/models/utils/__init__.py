13d097163212deb02a8b1e5a01bb1fe14bab0baf
eublefar <evil.unicorn1@gmail.com>
2022-04-03 12:40:23 +0200

    Dtype map from onnx type strings

--- npc_engine/models/utils/__init__.py
+++ npc_engine/models/utils/__init__.py
@@ -1,1 +1,11 @@
 """Utility package."""
+import numpy as np
+
+
+# Map from onnx type string to numpy dtype
+DTYPE_MAP = {
+    "tensor(int64)": np.int64,
+    "tensor(float)": np.float32,
+    "tensor(double)": np.float64,
+    "tensor(int32)": np.int32,
+}
