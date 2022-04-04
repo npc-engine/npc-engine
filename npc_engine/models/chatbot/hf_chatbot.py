HF Chatbot attention mask correct shape

npc_engine/models/chatbot/hf_chatbot.py
@@ -88,6 +88,7 @@ class HfChatbot(ChatbotAPI):
         inputs = self.create_starter_inputs(prompt)
         utterance = []
         for i in range(self.max_steps):
+            print(f"Step {i}")
             o = self.model.run(None, inputs,)
             logit = o[0][0, -1, :]
             if i < self.min_length:
@@ -98,7 +99,7 @@ class HfChatbot(ChatbotAPI):
                 outp.name: o[i] for i, outp in enumerate(self.model.get_outputs())
             }
             inputs = self.update_inputs_with_results(inputs, result_dict, token)
-
+            print({name: inp.shape for name, inp in inputs.items()})
             if token == self.eos_token_id:
                 break
         return self.tokenizer.decode(utterance, skip_special_tokens=True)
@@ -186,6 +187,10 @@ class HfChatbot(ChatbotAPI):
             inputs[ids_name] = np.asarray(
                 [decoded_token], dtype=self.dtypes[ids_name]
             ).reshape([1, -1])
+            inputs[att_mask_name] = np.ones(
+                [1, inputs[att_mask_name].shape[-1] + 1],
+                dtype=self.dtypes[att_mask_name],
+            )
             for inp in self.model_inputs:
                 if "past_key_values" in inp.name:
                     inputs[inp.name] = results[