817f032aedeba1e333a88e844349389014de4cd6
Igor Zmitrovich <izmitrowicz@gmail.com>
2022-10-24 20:44:55 +0200

    Update changelog and version

--- npc_engine/version.py
+++ npc_engine/version.py
@@ -3,5 +3,5 @@
 
 """This module contains project version information."""
 
-__version__ = "0.1.7"  #: the working version
-__release__ = "0.1.7"  #: the release version
+__version__ = "0.1.8"  #: the working version
+__release__ = "0.1.8"  #: the release version
